0be06110e454abbdfdc1e9d9008fd0d13d0280b0
Peter Ching & Shiraj Ganguly <pair+pc3sq.shirajg@devbootcamp.com>
2014-09-25 17:12:18 -0400

    Fix right_answer alias declaration

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :quiz
-  belongs_to :right_answer, class: "Track" #alias as right answer
+  belongs_to :right_answer, class_name: :Track , foreign_key: "track_id"
   has_many :wrongchoices
   has_many :useranswers
 end
