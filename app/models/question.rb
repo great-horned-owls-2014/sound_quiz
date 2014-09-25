d3bc19e32b65d49f2944268a36ba2ce2904852b5
Peter Ching & Shiraj Ganguly <pair+pc3sq.shirajg@devbootcamp.com>
2014-09-25 15:56:11 -0400

    Fix naming of Useranswers models and migrations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :quiz
   belongs_to :right_answer, class: "Track" #alias as right answer
+  has_many :wrongchoices
 end
--- app/models/user_answer.rb
+++
@@ -1,6 +0,0 @@
-class UserAnswer < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :user_decision, class: "Track" #alias user_decision
-  belongs_to :question
-
-end
