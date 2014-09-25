460deff9d76d18052bc6d1b132ddf6afc1d99f64
Peter Ching & Shiraj Ganguly <pair+pc3sq.shirajg@devbootcamp.com>
2014-09-25 17:53:08 -0400

    Add seed file for Active Record tests only.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "Peter")
+
+Artist.create(name: "Shigoom")
+Artist.first.tracks << Track.create(name: "Shiggitty")
+Artist.first.tracks << Track.create(name: "Shig One")
+Artist.first.tracks << Track.create(name: "Shig Two")
+Artist.first.tracks << Track.create(name: "Shig Three")
+
+Artist.first.quizzes << Quiz.create(difficulty_level: 1)
+Quiz.first.questions << Question.create(right_answer: Track.first)
+Quiz.first.questions << Question.create(right_answer: Track.second)
+Quiz.first.questions << Question.create(right_answer: Track.third)
+
+Question.first.wrong_choices << WrongChoice.create(track: Track.second)
+Question.first.wrong_choices << WrongChoice.create(track: Track.third)
+Question.first.wrong_choices << WrongChoice.create(track: Track.fourth)
+
+User.first.user_answers << UserAnswer.create(track_id: 2, question_id: 1, response_time: 0.1 )
