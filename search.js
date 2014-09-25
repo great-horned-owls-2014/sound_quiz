0f308eb5b99b2c5e2e377eeb35dc895e1fb81ab1
Deepak in the house.  & Victor Y <pair+deepakswami07.lunacraz@devbootcamp.com>
2014-09-25 15:21:24 -0400

    Create ajax call to get songs of specific artist

--- search.js
+++ search.js
@@ -1,24 +1,58 @@
-// https://itunes.apple.com/search?term=massive+attack&attribute=allArtistTerm
-searchUrl= 'https://itunes.apple.com/search?entity=musicArtist&term='
+// https://itunes.apple.com/search?attribute=allArtistTerm&term=massive+attack
+var songSearchUrl = 'https://itunes.apple.com/search?attribute=allArtistTerm&entity=song&term='
+var artistSearchUrl= 'https://itunes.apple.com/search?entity=musicArtist&term='
 var testResponse;
+var artistName;
+var artistId;
+var songList = [];
+
+var test;
 
 $(document).ready(function(){
   $('form').submit(function(event){
     event.preventDefault();
     searchTerm = $('#searchterm').val();
-      $.ajax({
-        url: searchUrl+searchTerm,
-        type: 'GET',
-        dataType: 'jsonp',
-        success: function(response){
-          testResponse = response;
-          for(var i=0; i<response['results'].length; i++){
-            $('#artistlist ul').append('<li><a href="'+ response['results'][i].artistLinkUrl +'">'+response['results'][i].artistName + '</a></li>');
-          }
-        },
-        failure: function(response){
-          console.log('Fail');
+    $.ajax({
+      url: artistSearchUrl+searchTerm,
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(response){
+        testResponse = response;
+        for(var i=0; i<response['results'].length; i++){
+          $('#artistlist ul').append('<li><a href="'+ response['results'][i].artistLinkUrl +'" data-artistid="'+response['results'][i].artistId+'">'+response['results'][i].artistName + '</a></li>');
         }
-      })
+      },
+      failure: function(response){
+        console.log('Fail');
+      }
+    })
   })
+
+  $('#artistlist').on('click', 'a', function(event){
+    event.preventDefault();
+    artistId = $(this).data('artistid');
+    artistName = $(this).text();
+    console.log(songSearchUrl + artistName);
+    $.ajax({
+      url: songSearchUrl + artistName,
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(response){
+        console.log(response);
+        test = response;
+        for(var i =0; i< response['results'].length; i++){
+          if (artistId === response['results'][i].artistId) {
+            songList.push(response['results'][i]);
+          }
+        }
+      },
+       failure: function(response){
+        console.log('Fail');
+
+      }
+
+    });
+
+  });
+
 });
