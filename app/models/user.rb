70f0fc66b128cc53d69b8eaf161695445b4e38c3
Peter Ching & Shiraj Ganguly <pair+pc3sq.shirajg@devbootcamp.com>
2014-09-26 19:23:08 -0400

    Remove redundant password prescence validation and comment out email regex validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :user_answers
-  has_secure_password
+  has_secure_password #takes care of password prescence validation
 
   # validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates_uniqueness_of :email
