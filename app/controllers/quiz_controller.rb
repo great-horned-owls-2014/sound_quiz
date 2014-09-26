9cc0d5643b42c98a6a676b4f43ba48f63b34b166
Shiraj Ganguly <shirajganguly@gmail.com>
2014-09-26 19:07:45 -0400

    Refactor quiz create route

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,17 +1,29 @@
 class QuizController < ApplicationController
-  def create
-    new_artist = Artist.create(name: params[:name], itunes_id: params[:id])
-    new_artist.quizzes << Quiz.create(difficulty_level: 1)
-    for  i in 0..(params[:list].length - 1 ) do
-      new_track = Track.create(
-        preview_url: params[:list][i.to_s]["previewUrl"],
-        art_url: params[:list][i.to_s]["artworkUrl100"],
-        name: params[:list][i.to_s]["trackName"]  )
-      new_artist.tracks << new_track
-    end
 
-    populate_quiz(new_artist, new_artist.quizzes.last.id)
+  def create
+    # artist & tracks, knows nothing of quiz
+    new_artist = Artist.new artist_attribs_from_params params
+    new_artist.tracks = params[:list].length.times.map do |i|
+      Track.new track_attribs_from_params params[:list][i.to_s]
+    end
+    new_artist.save!
+
+    quiz = create_first_quiz_for(new_artist)
+    quiz.save!
 
     render :json => create_frontend_quiz(new_artist, new_artist.quizzes.last.id)
   end
+
+  def artist_attribs_from_params params
+    { name: params[:name], itunes_id: params[:id] }
+  end
+
+  def track_attribs_from_params track_from_params
+    {
+      preview_url: track_from_params["previewUrl"],
+      art_url: track_from_params["artworkUrl100"],
+      name: track_from_params["trackName"]
+    }
+  end
+
 end
