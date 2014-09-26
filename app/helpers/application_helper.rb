96a71f0c97297ab685acb4506d8eb708c06b6505
Shiraj Ganguly <shirajganguly@gmail.com>
2014-09-26 19:08:03 -0400

    Refactor helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,35 +8,41 @@ module ApplicationHelper
 
     artist.quizzes.find(quiz_id).questions.each_with_index do |question, array_index|
       human_index = (array_index + 1).to_s
-      quiz[("question#{human_index}").to_sym] = {}
-      quiz[("question#{human_index}").to_sym][:db_id] = question.id
-      quiz[("question#{human_index}").to_sym][:choices] = []
-      quiz[("question#{human_index}").to_sym][:choices] << question.right_answer
-      question.wrong_choices.each do |choice|
-        quiz[("question#{human_index}").to_sym][:choices] << choice.track
-      end
-      quiz[("question#{human_index}").to_sym][:choices].shuffle
+      question_key = ("question_#{human_index}").to_sym
+      quiz[question_key] = make_question_hash question
     end
 
-    return quiz
+    quiz
   end
 
-  def populate_quiz(artist, quiz_id)
-    answer_tracks = artist.tracks.first(5)
+  def make_question_hash question
+    question_hash = {}
+    question_hash[:db_id] = question.id
+    question_hash[:choices] = [ question.right_answer ] + question.wrong_choices.map(&:track)
+    question_hash[:choices] = question_hash[:choices].shuffle
+    question_hash
+  end
 
-    while artist.quizzes.find(quiz_id).questions.length < 5
-      artist.quizzes.find(quiz_id).questions << Question.create(right_answer: answer_tracks.shift)
-    end
+  def create_first_quiz_for artist
+    artist.quizzes = [ Quiz.new(difficulty_level: 1) ]
 
-    artist.quizzes.find(quiz_id).questions.each do |question|
-      choices = artist.tracks.first(10)
-      while question.wrong_choices.length < 3
-        potential_wrong_answer = choices.pop
-        if potential_wrong_answer != question.right_answer
-          question.wrong_choices << WrongChoice.create(track: potential_wrong_answer )
-        end
-      end
-    end
+     answer_tracks = artist.tracks.first(5)
+
+     while artist.quizzes.first.questions.length < 5
+       artist.quizzes.first.questions << Question.new(right_answer: answer_tracks.shift)
+     end
+
+     artist.quizzes.first.questions.each do |question|
+       choices = artist.tracks.first(10)
+       while question.wrong_choices.length < 3
+         potential_wrong_answer = choices.pop
+         if potential_wrong_answer != question.right_answer
+           question.wrong_choices << WrongChoice.new(track: potential_wrong_answer )
+         end
+       end
+     end
+
+     artist.quizzes.first
   end
 
 
