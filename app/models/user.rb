5ef547901dea861a57f7fab350886ef46cae7621
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-27 16:15:35 -0400

    Refactor user score logic for readability.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ActiveRecord::Base
   has_many :user_answers
 
+  def quiz_score (quiz_id, user_answers_arr, time_arr)
+
+    time_elapsed_for_quiz = time_arr[-1] - time_arr[0]
+
+    inverse_time_elapsed = 1.0 / time_elapsed_for_quiz
+
+    raw_score = percentage_correct_for_current_quiz(quiz_id, user_answers_arr) * inverse_time_elapsed
+
+    meaningful_score = (raw_score * 1000000000).to_i
+
+  end
+
+  def all_time_percentage_correct # returns percentage correct vs all questions ever taken
+    total_attempts_ever = self.user_answers.count.to_f
+
+    total_successful_attempts_ever = self.user_answers.joins(:question).where('user_answers.track_id = questions.track_id').count.to_f
+
+    total_successful_attempts_ever / total_attempts_ever
+  end
+
+  private
+
   def percentage_correct_for_current_quiz (quiz_id, user_answers_arr)
     total_attempts_for_this_quiz = 5.0
     total_successful_attempts_for_this_quiz = 0.0
@@ -17,24 +39,6 @@ class User < ActiveRecord::Base
 
     total_successful_attempts_for_this_quiz / total_attempts_for_this_quiz
   end
-
-  def all_time_percentage_correct # returns percentage correct vs all questions ever taken
-    total_attempts_ever = self.user_answers.count.to_f
-
-    total_successful_attempts_ever = self.user_answers.joins(:question).where('user_answers.track_id = questions.track_id').count.to_f
-
-    total_successful_attempts_ever / total_attempts_ever
-  end
-
-
-
-  # def average_of_all_time_percentage_correct_for_quiz (quiz_id)
-
-  #   self.user_answers.joins(:question).where('user_answers.track_id = questions.track_id' && 'questions.quiz_id = ?', quiz_id)
-
-  # end
-
-  # user_answers.joins(:question).where(user_answers.track_id = questions.track_id' && 'questions.quiz_id = ?', quiz_id)
 
 end
 
