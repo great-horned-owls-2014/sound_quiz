cce43444869fd158d7d34d6a2ea5cc1d992924ed
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-27 14:34:51 -0400

    Add #percentage_correct_for_current_quiz in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,31 @@
 class User < ActiveRecord::Base
   has_many :user_answers
 
-  def all_time_percentage_correct # gives ration/percentage of correct vs all questions ever taken
+  def percentage_correct_for_current_quiz (quiz_id, user_answers_arr)
+    total_attempts_for_this_quiz = 5.0
+    total_successful_attempts_for_this_quiz = 0.0
+    quiz = Quiz.find(quiz_id)
+    quiz_questions = quiz.questions
 
+    quiz_questions.each do | question |
+      user_answers_arr.each do | user_answer |
+        if question.right_answer.id == user_answer.user_decision.id
+          total_successful_attempts_for_this_quiz += 1
+        end
+      end
+    end
+
+    total_successful_attempts_for_this_quiz / total_attempts_for_this_quiz
+  end
+
+  def all_time_percentage_correct # returns percentage correct vs all questions ever taken
     total_attempts_ever = self.user_answers.count.to_f
 
     total_successful_attempts_ever = self.user_answers.joins(:question).where('user_answers.track_id = questions.track_id').count.to_f
 
     total_successful_attempts_ever / total_attempts_ever
-
   end
 
-  # def percentage_correct_for_current_quiz
-
-
-
-  # end
 
 
   # def average_of_all_time_percentage_correct_for_quiz (quiz_id)
@@ -24,9 +34,12 @@ class User < ActiveRecord::Base
 
   # end
 
-
-
-
   # user_answers.joins(:question).where(user_answers.track_id = questions.track_id' && 'questions.quiz_id = ?', quiz_id)
 
 end
+
+#============
+# QUESTIONS
+#============
+
+# user_answers should have a boolean that says if it's correct or not.
