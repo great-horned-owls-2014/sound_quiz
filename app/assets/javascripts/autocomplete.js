e574cd43b9d73ad1c11806e81c3599122d37fd4d
Peter Ching <peter.ching@gmail.com>
2014-09-28 16:29:44 -0400

    Add autocomplete search that sends artist name with its ajax request to server.

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
   $('#artist_name').on('railsAutocomplete.select', function(event, data){
+    event.preventDefault();
     console.log(data);
     var artist_id = data.item.id;
     location.href = ('/artists/' + artist_id);
+    // var artistRoute = '/artists/' + artist_id
+    // $(this).append('<a class="artist" href=' + artistRoute + '></a>');
     //location.href = 'http://google.com'
   });
 })
