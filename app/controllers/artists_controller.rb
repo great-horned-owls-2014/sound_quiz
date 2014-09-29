f9171a5d5a2d3e2411650bedcc106b6aad8d7078
Peter Ching <peter.ching@gmail.com>
2014-09-29 18:29:39 -0400

    Refactor view and controller to query rather than iterate for leaderboard information.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,14 +9,8 @@ class ArtistsController < ApplicationController
   def show
     artist_id = params[:id]
   	@artist = Artist.find(artist_id)
-  	@user_taken = Hash.new(0)
-    @artist.taken_quizzes.each do |takenquiz|
-      @user_taken[takenquiz.user.id] += 1
-    end
 
     avg_scores = TakenQuiz.where(artist_id: artist_id).joins(:user).group('users.id').group('users.username').average(:score)
-
-    @sorted_avg_scores = avg_scores.sort_by {|k,v| v}.reverse
+    @sorted_avg_scores = avg_scores.sort_by {|user_info, avg_score| avg_score}.reverse
   end
-
 end
