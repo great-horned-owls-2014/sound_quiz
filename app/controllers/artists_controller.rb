a49c94560b02b9ec31471bd6cc6edb0824cd571b
Peter Ching <peter.ching@gmail.com>
2014-09-29 17:38:33 -0400

    Use AREL to generate an array of user_id, username, and avergae scores hashed for a particular artist_id.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,14 +7,16 @@ class ArtistsController < ApplicationController
   end
 
   def show
-  	@artist = Artist.find(params[:id])
+    artist_id = params[:id]
+  	@artist = Artist.find(artist_id)
   	@user_taken = Hash.new(0)
     @artist.taken_quizzes.each do |takenquiz|
       @user_taken[takenquiz.user.id] += 1
     end
 
-    @user_score = TakenQuiz.where(artist_id: 1).joins(:user).group('users.username').average(:score)
+    avg_scores = TakenQuiz.where(artist_id: artist_id).joins(:user).group('users.id').group('users.username').average(:score)
 
+    @sorted_avg_scores = avg_scores.sort_by {|k,v| v}.reverse
   end
 
 end
