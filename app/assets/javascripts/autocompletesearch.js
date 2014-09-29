460efb39019cf4aed582aff5905cac553a0c0908
Peter Ching <peter.ching@gmail.com>
2014-09-28 22:16:34 -0400

    Place search logic in search.js into autocompletesearch.js.

--- app/assets/javascripts/autocompletesearch.js
+++ app/assets/javascripts/autocompletesearch.js
@@ -1,6 +1,58 @@
 $(document).ready(function(){
 
+  //invariants
+  var songSearchUrl = 'https://itunes.apple.com/search?attribute=allArtistTerm&entity=song&limit=100&term=';
+  var artistSearchUrl= 'https://itunes.apple.com/search?entity=musicArtist&limit=5&term=';
+  
+  //results from iTunes APIs
+  var artistName = '';
   var artistId = '';
+  var artistObjectResults = null;
+
+  function createSongList(artistObjectResults){
+    songArray = [];
+    for(var i =0; i< artistObjectResults['results'].length; i++){
+      if (artistId === artistObjectResults['results'][i].artistId) {
+        songArray.push(createSongObject( artistObjectResults['results'][i] ));
+      }
+    }
+    return songArray;
+  }
+
+  function createSongObject(itunesObject){
+    filteredObject = { 'artworkUrl100': itunesObject.artworkUrl100,
+                        'previewUrl': itunesObject.previewUrl,
+                        'trackName': itunesObject.trackName};
+    return filteredObject;
+  }
+
+  function dbSend(artistName, artistId, songArray){
+    $.ajax({
+      url: '/quiz/create',
+      type: 'POST',
+      data: {name: artistName, id: artistId, list: songArray},
+      success: function(response){
+
+        quiz = scrubQuestionChoices(response);
+        initializeGame();
+      },
+       failure: function(response){
+        console.log('Fail');
+      }
+    });
+  }
+
+  function scrubQuestionChoices(quiz){
+    for(var i=1; i <= (Object.keys(quiz).length - numOfNonQuestions); i++){
+      for ( var j=0; j < quiz['question_'+i]['choices'].length; j++){
+        delete (quiz['question_'+i]['choices'][j].preview_url);
+        delete (quiz['question_'+i]['choices'][j].created_at);
+        delete (quiz['question_'+i]['choices'][j].updated_at);
+      }
+    }
+    return quiz;
+  }
+
 
   $('#artistsearchterm').autocomplete({
     // minLength: 3,
@@ -8,21 +60,20 @@ $(document).ready(function(){
       // calling autocomplete creates a request object containing the terms you input into the selector.
       // not sure what the response argument is
     source: function(request, response) {
-        
-        // console.log("Here is the source response: ")
-        // console.log(response);
         var artistSearchTerms = $('#artistsearchterm').val();
         $.ajax({
-            url: "https://itunes.apple.com/search?entity=musicArtist&limit=5&term=" + artistSearchTerms,
+            url: artistSearchUrl + artistSearchTerms,
             dataType: "jsonp",
             data: {term: request.term},
-            success: function( data ) {
-              response($.map( data.results, function( item ) {
-                artistId = item.artistId
-                var artistLabel = item.artistName + "     / GENRE: " + item.primaryGenreName
+            success: function( artistObject ) {
+              artistObjectResults = artistObject.results
+              response($.map( artistObjectResults, function( item ) {
+                // artistObject = item
+                artistName = item.artistName;
+                artistId = item.artistId;
+                var artistLabel = item.artistName + "     / GENRE: " + item.primaryGenreName;
                 return {
-                    label: artistLabel,
-                    // value: item.artistId,
+                    label: artistLabel
                 }
               }));
             },
@@ -32,17 +83,22 @@ $(document).ready(function(){
             }
         });
       },
+
     select: function(event, ui){
       event.preventDefault();
       $.ajax({
-        url: "/artists/",
-        datatype: 'json'
-      }).done(function(response){
-        console.log("Success! It worked! You sent the artistId: " + artistId)
-      }).fail(function(failResponse){
+        url: songSearchUrl + artistName,
+        type: 'GET',
+        dataType: 'jsonp'
+      })
+      .done(function(songObject){
+        selectedArtistSongList = createSongList(songObject);
+        dbSend(artistName, artistId, selectedArtistSongList);
+      })
+      .fail(function(failResponse){
         console.log("Ajax failed. Here was the response from the server: " + failResponse);
       })
     }
-  });
 
+  });
 })
