fca3bddfcd32ca8157ea09e25c9d893c54e95fd0
Peter Ching <peter.ching@gmail.com>
2014-09-29 16:31:22 -0400

    Add user and scores hash using AREL.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,7 +9,12 @@ class ArtistsController < ApplicationController
   def show
   	@artist = Artist.find(params[:id])
   	@user_taken = Hash.new(0)
-    @artist.taken_quizzes.each { |takenquiz| @user_taken[takenquiz.user.id] += 1}
+    @artist.taken_quizzes.each do |takenquiz|
+      @user_taken[takenquiz.user.id] += 1
+    end
+
+    @user_score = TakenQuiz.where(artist_id: 1).joins(:user).group('users.username').average(:score)
+
   end
 
 end
