8cdef965498633660f259dee6f4ef5c38685fadb
Peter Ching <peter.ching@gmail.com>
2014-09-30 14:22:48 -0400

    Make quiz appear when you click on quiz link in scoreless leaderboard.

--- app/assets/javascripts/first_quiz.js
+++ app/assets/javascripts/first_quiz.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 
   $('[data-artist-id]').on('click', function(e){
     e.preventDefault();
+    alert("I'm here!");
     var artistId = $(this).attr('data-artist-id');
+    console.log(artistId);
     $.ajax({
           url: '/artists/' + artistId + '/first_quiz',
           type: 'GET',
           success: function(response){
-
+            alert('Success! Something else wrong?');
             quiz = scrubQuestionChoices(response);
             initializeGame();
           },
