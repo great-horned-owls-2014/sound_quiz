d916327ea149b385548bd1259c03af3c8cac8620
Shiraj Ganguly <shirajganguly@gmail.com>
2014-10-01 18:04:04 -0400

    Moved frontend_quiz_creation to quiz controller

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -13,6 +13,30 @@ class QuizController < ApplicationController
     artist.save!
   end
 
+  def create_frontend_quiz(artist, quiz_id)
+    quiz = {}
+    quiz[:artist] = artist.name
+    quiz[:id] = artist.id
+    quiz[:itunes_id] = artist.itunes_id
+
+    Quiz.find(quiz_id).questions.each_with_index do |question, array_index|
+      human_index = (array_index + 1).to_s
+      question_key = ("question_#{human_index}").to_sym
+      quiz[question_key] = make_question_hash question
+    end
+
+    quiz
+  end
+
+  def make_question_hash question
+    question_hash = {}
+    question_hash[:db_id] = question.id
+    question_hash[:player_url] = question.right_answer.preview_url
+    question_hash[:choices] = [ question.right_answer ] + question.wrong_choices.map(&:track)
+    question_hash[:choices] = question_hash[:choices].shuffle
+    question_hash
+  end
+
   def create
     # artist & tracks, knows nothing of quiz
     artist_itunes_id = params[:id]
