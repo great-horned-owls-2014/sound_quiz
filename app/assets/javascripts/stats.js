2d065920e01e844cc39e2c99a324ebfed7c33250
Peter Ching <peter.ching@gmail.com>
2014-10-01 10:15:06 -0400

    Delegate the same artist click so that listener can be bound to it

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -1,27 +1,10 @@
 var idLookupUrl = 'https://itunes.apple.com/lookup?id='
 
 $(document).ready(function(){
-	$('[data-track-id]').each(function(index, element) {
-		itunesId = element.getAttribute('data-track-id')
-	   $.ajax({
-	     url: idLookupUrl+itunesId,
-	     type: 'GET',
-	     dataType: 'jsonp',
-	     success: function(response){
-	     	itunesObject=response['results'][0]
-	      element.innerHTML = '<a target = "_blank" href="'+itunesObject.trackViewUrl+'">'+itunesObject.trackName+'</a> <img class="itunesicon" src="itunes.png">';
-	     },
-	     failure: function(response){
-	       console.log(response);
-	       console.log("failure");
-	     }
-	   });
-	});
 
 	// this part is really really freaking buggy, maybe take out
- 	$('#playartistagain').on('click', function(event){
+ 	$('body').delegate('#playartistagain', 'click', function(event){
  		event.preventDefault();
- 		console.log('HELLO TEST');
  		$('#loadingscreen').slideDown(1000);
  		$('.returnedstats').hide();
  		$('.gamequestions').remove();
