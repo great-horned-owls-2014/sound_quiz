c66b6137abdc19638bdbcc9881be73c460983d6b
Shiraj Ganguly <shirajganguly@gmail.com>
2014-10-01 20:18:45 -0400

    Update styling of actual quizzes

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -107,7 +107,7 @@ function initializeGame(){
 function generateQuestionDiv(question){
   divString = '<div  style="display: none;" class="gamequestions" data-questionId="'+ question['db_id']  +'">';
   for(var i=0; i< question.choices.length; i++){
-    divString += '<button style="display:block;" data-questionId="' + question['db_id'] + '" data-choiceId="'+ question.choices[i].id +'" class="answer-button" type="button">'+question.choices[i].name+'</button>';
+    divString += '<button data-questionId="' + question['db_id'] + '" data-choiceId="'+ question.choices[i].id +'" class="answer-button" type="button">'+question.choices[i].name+'</button>';
   }
   divString += '<button style="display:none;" data-questionId="' + question['db_id'] + '" data-choiceId="-1" class="answer-button" type="button"></button>';
   divString += songPlayer(question.player_url)+'</div>';
